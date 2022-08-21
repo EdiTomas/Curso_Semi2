63db1198644780d0f59e4b371e895c859d0cc555
Edi Tomas <2370557320101@ingenieria.usac.edu.gt>
2022-08-20 20:09:15 -0600

    Delete
    Deleted a  code fragment  commented.

--- Practica1/DB/BorrarModel.sql
+++ Practica1/DB/BorrarModel.sql
@@ -1,22 +1,3 @@
-/*
-                   IF   OBJECT_ID('Tsunami') IS NOT NULL 
-                    AND OBJECT_ID('Country') IS NOT NULL
-                    AND OBJECT_ID('Tiempo') IS NOT NULL
-                    AND OBJECT_ID('damage') IS NOT NULL
-                    AND OBJECT_ID('temptsunami') IS NOT  NULL
-                    AND OBJECT_ID('Desastres') IS NOT NULL
-                       
-               BEGIN
-               DROP TABLE Desastres
-               DROP TABLE temptsunami
-               DROP TABLE Tsunami
-               DROP TABLE Country
-               DROP TABLE Tiempo
-               DROP TABLE damage
-               
-               
-END
-*/
 
                  IF   OBJECT_ID('Tsunami') IS NOT NULL 
                     AND OBJECT_ID('temptsunami') IS NOT  NULL
